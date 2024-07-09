5589cad3546055e1a921ccaf3831ef9ef8c89ef5
nisha8291 <ynisha509@gmail.com>
2024-07-09 16:24:08 +0530

    NY(regex ui changes)

--- src/features/MyProfile/Components/MyProfileBankGSTComponents/BankInfoComponent.js
+++ src/features/MyProfile/Components/MyProfileBankGSTComponents/BankInfoComponent.js
@@ -1,3 +1,275 @@
+// import { useState, useEffect } from "react";
+// import warning from "../../../../assets/images/warningGif.gif";
+// import React from "react";
+// import Select from "react-select";
+// import CloseIcon from "@mui/icons-material/Close";
+
+// function BankInfo({ onClose }) {
+//   const [currentDate, setCurrentDate] = useState("");
+//   const [bankAccountNumber, setBankAccountNumber] = useState("");
+//   const [branchName, setBranchName] = useState("");
+//   const [ifsc, setIfsc] = useState("");
+//   const [selectedOption, setSelectedOption] = useState(null);
+//   const [inputDisabled, setInputDisabled] = useState(false);
+//   const [validIfsc, setValidIfsc] = useState(true);
+//   const [error, setError] = useState("");
+//   const [errors, setErrors] = useState({});
+
+//   const [bankDetails, setBankDetails] = useState({
+//     bankName: "",
+//     bankAccountNumber: "",
+//     branchName: "",
+//     ifsc: "",
+//     currentDate: "",
+//   });
+
+//   console.log({ bankDetails });
+
+//   useEffect(() => {
+//     const date = new Date();
+//     const formattedDate = date.toLocaleDateString();
+//     setCurrentDate(formattedDate);
+//     setBankDetails((prevDetails) => ({
+//       ...prevDetails,
+//       currentDate: formattedDate,
+//     }));
+//   }, []);
+
+//   const bankOptions = [
+//     {
+//       value: "ap_mahesh_cooperative_urban_bank_limited",
+//       label: "AP MAHESH COOPERATIVE URBAN BANK LIMITED",
+//     },
+//     {
+//       value: "abhyudaya_cooperative_bank_limited",
+//       label: "ABHYUDAYA COOPERATIVE BANK LIMITED",
+//     },
+//     {
+//       value: "abhyudaya_mahila_urban_cooperative_bank_limited",
+//       label: "ABHYUDAYA MAHILA URBAN COOPERATIVE BANK LIMITED",
+//     },
+//     { value: "abu_dhabi_commercial_bank", label: "ABU DHABI COMMERCIAL BANK" },
+//     {
+//       value: "ace_cooperative_bank_limited",
+//       label: "ACE COOPERATIVE BANK LIMITED",
+//     },
+//     {
+//       value: "adarsh_cooperative_bank_limited",
+//       label: "ADARSH COOPERATIVE BANK LIMITED",
+//     },
+//     {
+//       value: "ahilyadevi_urban_cooperative_bank_limited",
+//       label: "AHILYADEVI URBAN COOPERATIVE BANK LIMITED",
+//     },
+//     {
+//       value: "ahmedabad_mercantile_cooperative_bank",
+//       label: "AHMEDABAD MERCANTILE COOPERATIVE BANK",
+//     },
+//     {
+//       value: "airtel_payments_bank_limited",
+//       label: "AIRTEL PAYMENTS BANK LIMITED",
+//     },
+//     {
+//       value: "akola_janata_commercial_cooperative_bank",
+//       label: "AKOLA JANATA COMMERCIAL COOPERATIVE BANK",
+//     },
+//     {
+//       value: "alapuzha_district_cooperative_bank",
+//       label: "ALAPUZHA DISTRICT COOPERATIVE BANK",
+//     },
+//     {
+//       value: "alavi_cooperative_bank_limited",
+//       label: "ALAVI COOPERATIVE BANK LIMITED",
+//     },
+//     { value: "allahabad_bank", label: "ALLAHABAD BANK" },
+//     {
+//       value: "allahabad_district_cooperative_bank_limited",
+//       label: "ALLAHABAD DISTRICT COOPERATIVE BANK LIMITED",
+//     },
+//     { value: "allahabad_up_gramin_bank", label: "ALLAHABAD UP GRAMIN BANK" },
+//     {
+//       value: "almora_urban_cooperative_bank_limited",
+//       label: "ALMORA URBAN COOPERATIVE BANK LIMITED",
+//     },
+//   ];
+
+//   const handleInputChange = (event) => {
+//     const { value } = event.target;
+//     const newValue = value.replace(/\D/g, "").slice(0, 18);
+
+//     setBankAccountNumber(newValue);
+//     setBankDetails((prevDetails) => ({
+//       ...prevDetails,
+//       bankAccountNumber: newValue,
+//     }));
+//   };
+
+//   const handleBranchNameInputChange = (event) => {
+//     const { value } = event.target;
+//     const newValue = value.replace(/[^a-zA-Z\s]/g, "");
+
+//     setBranchName(newValue);
+//     setBankDetails((prevDetails) => ({
+//       ...prevDetails,
+//       branchName: newValue,
+//     }));
+//   };
+
+//   const validateIfsc = (value) => {
+//     const ifscRegex = /^[A-Z]{4}0[0-9]{6}$/;
+
+//     let upperCaseValue = value.toUpperCase();
+//     const firstPart = upperCaseValue.slice(0, 4).replace(/[^A-Z]/g, "");
+//     let secondPart = "";
+//     if (upperCaseValue.length > 4) {
+//       secondPart = upperCaseValue.slice(4, 5).replace(/[^0]/g, "");
+//     }
+//     let thirdPart = "";
+//     if (upperCaseValue.length > 5) {
+//       thirdPart = upperCaseValue.slice(5).replace(/[^A-Z0-9]/g, "");
+//     }
+
+//     const filteredValue = (firstPart + secondPart + thirdPart).slice(0, 11);
+//     setIfsc(filteredValue);
+//     setBankDetails((prevDetails) => ({
+//       ...prevDetails,
+//       ifsc: filteredValue,
+//     }));
+
+//     let specificError = "";
+//     if (filteredValue.length !== 11) {
+//       specificError = "IFSC code must be 11 characters";
+//     } else if (!/^[A-Z]{4}/.test(filteredValue)) {
+//       specificError = "First four characters must be letters";
+//     } else if (filteredValue[4] !== "0") {
+//       specificError = "Fifth character must be zero";
+//     } else if (!/[0-9]{6}$/.test(filteredValue)) {
+//       specificError = "Last six characters must be digits";
+//     }
+
+//     setErrors((prevErrors) => ({
+//       ...prevErrors,
+//       ifsc: specificError || "",
+//     }));
+
+//     return ifscRegex.test(filteredValue);
+//   };
+
+//   const handleIfscInputChange = (e) => {
+//     const { value } = e.target;
+//     setValidIfsc(validateIfsc(value));
+//   };
+
+//   const handleChange = (selectedOption) => {
+//     setSelectedOption(selectedOption);
+//     setBankDetails((prevDetails) => ({
+//       ...prevDetails,
+//       bankName: selectedOption.label,
+//     }));
+//   };
+
+//   const isFormValid = () => {
+//     return (
+//       !errors.bankAccountNumber &&
+//       bankAccountNumber &&
+//       branchName &&
+//       ifsc &&
+//       selectedOption
+//     );
+//   };
+
+//   const handleSave = () => {
+//     console.log(bankDetails);
+//   };
+
+//   return (
+//     <>
+
+//       <CloseIcon onClick={onClose} />
+//       <div className="flex justify-between w-full sm:w-[68%] ">
+//         <label htmlFor="field11" className="w-[29%]  ">
+//           Bank Name:
+//         </label>
+
+//         <Select
+//           value={selectedOption}
+//           onChange={handleChange}
+//           options={bankOptions}
+//           placeholder="Select bank"
+//           className="size-10 w-[64%] mr-[57px] ml-4 border border-black rounded-md"
+//         />
+
+//         <div className="flex  w-40 mr-10">
+//           <span className=" p-1 border  rounded-2xl border-gray-500 bg-sky-200">
+//             Date: {currentDate}
+//           </span>
+//         </div>
+//       </div>
+
+//       <div className="flex justify-between w-full sm:w-[50%]  ">
+//         <label htmlFor="field22" className="">
+//           Bank Account Number:
+//         </label>
+//         <img src={warning} className=" h-6 w-6 " />
+
+//         <input
+//           type="text"
+//           name="field22"
+//           value={bankAccountNumber}
+//           onChange={handleInputChange}
+//           className="border border-black p-2 w-[67%] rounded-md "
+//           disabled={inputDisabled}
+//         />
+//       </div>
+
+//       <div className="flex justify-between w-full sm:w-[50%] ">
+//         <label htmlFor="field33" className="w-1/3">
+//           Branch Name:
+//         </label>
+//         <img src={warning} className=" h-6 w-6 mr-4  " />
+//         <input
+//           type="text"
+//           name="field33"
+//           value={branchName}
+//           onChange={handleBranchNameInputChange}
+//           className="border border-black p-2 w-[83%] rounded-md"
+//         />
+//       </div>
+
+//       <div className="flex justify-between w-full sm:w-[64%]   ">
+//         <label htmlFor="field44" className=" w-1/3 ">
+//           IFSC:
+//         </label>
+//         <img src={warning} className=" h-6 w-6 mr-4" />
+//         <input
+//           type="text"
+//           name="field44"
+//           value={ifsc}
+//           onChange={handleIfscInputChange}
+//           className="border border-black p-2 w-[83%] mr-4 rounded-md "
+//         />
+//         {errors.ifsc && <span style={{ color: "red" }}>{errors.ifsc}</span>}
+//         <div className="w-[30%] flex gap-1">
+//           <button
+//             onClick={handleSave}
+//             disabled={!isFormValid()}
+//             className={`border border-blue-400  rounded-2xl w-28 h-10 ml-10  ${
+//               isFormValid()
+//                 ? "bg-gray-300 text-blue-600"
+//                 : "bg-gray-200 text-gray-400 cursor-not-allowed"
+//             }`}
+//           >
+//             Instantly Verify
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default BankInfo;
+
+
 import { useState, useEffect } from "react";
 import warning from "../../../../assets/images/warningGif.gif";
 import React from "react";
--- src/features/MyProfile/Components/MyProfileBankGSTComponents/GST.js
+++ src/features/MyProfile/Components/MyProfileBankGSTComponents/GST.js
@@ -1,3 +1,244 @@
+// import { useState, useEffect } from "react";
+// import CloseIcon from "@mui/icons-material/Close";
+// import warning from "../../../../Images/warningGif.gif";
+// import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
+// // import { Select } from "@mui/material";
+// import Select from "react-select";
+
+// const taxationOptions = [
+//   { value: "GST", label: "GST" },
+//   { value: "VAT", label: "VAT" },
+//   { value: "CST", label: "CST" },
+// ];
+
+// function GST({ onClose }) {
+//   const [panNo, setPanNo] = useState("");
+//   const [ValidPan, setValidPan] = useState(true);
+//   const [ValidGst, setValidGst] = useState(true);
+//   const [taxationType, setTaxationType] = useState(null);
+//   const [gstNumber, setGstNumber] = useState("");
+//   const [panError, setPanError] = useState("");
+//   const [gstError, setGstError] = useState("");
+//   const [inputDisabled, setInputDisabled] = useState(false);
+
+//   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+//   useEffect(() => {
+//     if (panNo && taxationType && gstNumber && !panError) {
+//       setIsButtonDisabled(false);
+//     } else {
+//       setIsButtonDisabled(true);
+//     }
+//   }, [panNo, taxationType, gstNumber, panError]);
+
+//   console.log({ panNo });
+
+//   // const handleInputChange = (e) => {
+//   //   const { value } = e.target;
+//   //   setValidPan(validatePanNo(value));
+//   // };
+
+//   // const validatePanNo = (value) => {
+//   //   debugger;
+//   //   const panRegex = /^[A-Z]{3}[P\d][A-Z]\d{4}[A-Z]$/;
+
+//   //   let upperCaseValue = value.toUpperCase();
+//   //   const firstPart = upperCaseValue.slice(0, 3).replace(/[^A-Z]/g, "");
+//   //   let fourthCharacter = "";
+//   //   if (upperCaseValue.length > 3) {
+//   //     fourthCharacter = upperCaseValue
+//   //       .charAt(3)
+//   //       .toUpperCase()
+//   //       .replace(/[^P]/g, "");
+//   //   }
+//   //   const fifthCharacter = upperCaseValue.slice(4, 5).replace(/[^A-Z]/g, "");
+//   //   const sixthToNinthCharacters = upperCaseValue
+//   //     .slice(5, 9)
+//   //     .replace(/[^0-9]/g, "");
+//   //   const tenthCharacter = upperCaseValue.slice(9).replace(/[^A-Z]/g, "");
+
+//   //   const filteredValue = (
+//   //     firstPart +
+//   //     fourthCharacter +
+//   //     fifthCharacter +
+//   //     sixthToNinthCharacters +
+//   //     tenthCharacter
+//   //   ).slice(0, 10);
+//   //   setPanNo(filteredValue);
+
+//   //   return panRegex.test(filteredValue);
+//   // };
+
+//   const handleInputChange = (event) => {
+//     debugger;
+//     const { value } = event.target;
+//     const upperCaseValue = value.toUpperCase();
+//     const filteredValue = upperCaseValue.replace(/[^A-Z0-9]/g, "");
+
+//     setPanNo(filteredValue);
+//     validatePanNo(filteredValue);
+//     setInputDisabled(false);
+//   };
+//   const validatePanNo = (value) => {
+//     debugger;
+//     const panRegex = /^[A-Z]{3}[P]{1}[A-Z]{1}[0-9]{4}[A-Z]{1}$/; ///^[A-Z]{3}[P\d][A-Z]\d{4}[A-Z]$/;
+//     const firstreg = /^[A-Z]{0,3}$/; // Regex to match exactly three uppercase letters
+//     const fourthCharacter = /^.{3}P.*/; ///[^P]/; ///^[A-Z]{4}P?$/;
+//     const fifthCharacter = /^[A-Z]{5}$/;
+//     const sixthToNinthCharacters = /^[0-9A-Z]{6,9}$/;
+//     const tenthCharacter = /^.{9}[a-zA-Z].*$/;
+//     let upperCaseValue = value.toUpperCase(); // Convert value to uppercase
+
+//     // const isValidPan = panRegex.test(upperCaseValue);
+
+//     const firstPart = upperCaseValue.slice(0, 3);
+//     if (!firstreg.test(firstPart)) {
+//       setPanError("Invalid PAN Number First three charter must be alphabet.");
+//     } else if (
+//       upperCaseValue.length > 3 &&
+//       !fourthCharacter.test(upperCaseValue)
+//     ) {
+//       setPanError("Invalid PAN Number Fourth character should be P.");
+//     } else if (
+//       upperCaseValue.length > 4 &&
+//       upperCaseValue.length <= 5 &&
+//       !fifthCharacter.test(upperCaseValue)
+//     ) {
+//       setPanError("Invalid PAN Number Fifth character should be a character.");
+//     } else if (
+//       upperCaseValue.length > 5 &&
+//       upperCaseValue.length <= 9 &&
+//       !sixthToNinthCharacters.test(upperCaseValue)
+//     ) {
+//       setPanError(
+//         "Invalid PAN Number characters from position 6 to 9 must be digits or alphabetic characters."
+//       );
+//     } else if (
+//       upperCaseValue.length > 9 &&
+//       upperCaseValue.length <= 10 &&
+//       !tenthCharacter.test(upperCaseValue)
+//     ) {
+//       setPanError("Invalid PAN Number Tenth Character should be a charcter.");
+//     } else {
+//       // No errors
+//       setPanError(""); // Assuming this means no error
+//     }
+
+//     setPanNo(upperCaseValue);
+//     return;
+//   };
+
+//   const validateGstNumber = (value) => {
+//     const gstRegex = /^[0-9]{2}[A-Z]{3}[P\d][A-Z]\d{4}[A-Z][0-9A-Z]Z[0-9A-Z]$/;
+
+//     let upperCaseValue = value.toUpperCase();
+//     const firstPart = upperCaseValue.slice(0, 2).replace(/[^0-9]/g, "");
+//     const panPart = upperCaseValue.slice(2, 12).replace(/[^A-Z0-9]/g, "");
+//     const thirteenthCharacter = upperCaseValue
+//       .slice(12, 13)
+//       .replace(/[^0-9A-Z]/g, "");
+//     const fourteenthCharacter = upperCaseValue
+//       .slice(13, 14)
+//       .replace(/[^Z]/g, "");
+//     const fifteenthCharacter = upperCaseValue
+//       .slice(14, 15)
+//       .replace(/[^0-9A-Z]/g, "");
+
+//     const filteredValue = (
+//       firstPart +
+//       panPart +
+//       thirteenthCharacter +
+//       fourteenthCharacter +
+//       fifteenthCharacter
+//     ).slice(0, 15);
+//     setGstNumber(filteredValue);
+
+//     return gstRegex.test(filteredValue);
+//   };
+
+//   const handleGstChange = (e) => {
+//     const { value } = e.target;
+//     setValidGst(validateGstNumber(value));
+//   };
+
+//   const handleTaxationTypeChange = (selectedOption) => {
+//     setTaxationType(selectedOption);
+//   };
+
+//   const handleSave = () => {};
+
+//   return (
+//     <>
+
+//       <CloseIcon onClick={onClose} />
+//       <div className="flex justify-between w-full sm:w-[64%] gap-4 ">
+//         <label className="w-1/3">PAN NO:</label>
+//         <img src={warning} className="h-6 w-6 mr-3" />
+//         <input
+//           type="text"
+//           maxLength="10"
+//           className="w-[60%]  mr-2 border border-black p-2 rounded-md size-10"
+//           value={panNo}
+//           onChange={(e) => handleInputChange(e, "panNo")}
+//         />
+//         {panError && <p className="text-red-500">{panError}</p>}
+//         <div className=" w-[30%] flex gap-2">
+//           <button
+//             onClick={handleSave}
+//             className={`border border-blue-400 rounded-2xl w-28 h-10 bg-gray-300 text-blue-600 ${
+//               isButtonDisabled ? "opacity-50 cursor-not-allowed" : ""
+//             }`}
+//             disabled={isButtonDisabled}
+//           >
+//             instant Verify
+//           </button>
+//           <img src={warning} className=" -top-2 h-6 w-6 " />
+//           <FileUploadRoundedIcon color="error" />
+//         </div>
+//       </div>
+
+//       <div className="flex justify-between w-full sm:w-[64%] gap-4 ">
+//         <label className="w-1/3">Taxation Type:</label>
+//         <img src={warning} className="h-6 w-6 mr-3" />
+
+//         <Select
+//           className="w-[62%] mr-48 border border-black rounded-md size-10"
+//           value={taxationType}
+//           onChange={handleTaxationTypeChange}
+//           options={taxationOptions}
+//         />
+//       </div>
+
+//       <div className="flex justify-between w-full sm:w-[64%] gap-4">
+//         <label className="w-1/3">GST Number & Document:</label>
+//         <img src={warning} className=" h-6 w-6 mr-3" />
+//         <input
+//           type="text"
+//           className="w-[62%] mr-2  border border-black p-2 rounded-md size-10"
+//           value={gstNumber}
+//           onChange={(e) => handleGstChange(e, "gstNumber")}
+//         />
+//         {gstError && <p className="text-red-500">{gstError}</p>}
+//         <div className=" w-[30%] flex gap-2">
+//           <button
+//             onClick={handleSave}
+//             className={`border border-blue-400 rounded-2xl  w-28 h-10 bg-gray-300 text-blue-600 ${
+//               isButtonDisabled ? "opacity-50 cursor-not-allowed" : ""
+//             }`}
+//             disabled={isButtonDisabled}
+//           >
+//             instant Verify
+//           </button>
+//           <img src={warning} className="  h-6 w-6  " />
+//           <FileUploadRoundedIcon color="error" />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default GST;
+
 import { useState, useEffect } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import warning from "../../../../Images/warningGif.gif";
